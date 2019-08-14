37ab589b3f8d39ae01bddb90d2643e3cc6b6276c
Marcos Vinicius <marcos.v.silva@live.com>
2019-08-14 16:37:35 -0300

    Implements analysis twitter realtime

--- sentiment_analysis/sentiment_analysis.py
+++ sentiment_analysis/sentiment_analysis.py
@@ -1,11 +1,27 @@
 from _consumer import Consumer
 from _persistence import Persistence
 import pprint
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
 
 
 class Main:
     def __init__(self):
         pass
+
+    @staticmethod
+    def analyst_tweets():
+        persistence = Persistence()
+
+        ds = [dict(item) for item in persistence.find_all()]
+        df = pd.DataFrame(ds)
+        cv = CountVectorizer()
+        count_matrix = cv.fit_transform(df.text)
+
+        word_count = pd.DataFrame(cv.get_feature_names(), columns=["word"])
+        word_count["count"] = count_matrix.sum(axis=0).tolist()[0]
+        word_count = word_count.sort_values("count", ascending=False).reset_index(drop=True)
+        print(word_count[:100])
 
     @staticmethod
     def consumer_twitter():
@@ -20,7 +36,9 @@ class Main:
             pprint.pprint(tweet)
 
 
+
 if __name__ == '__main__':
     main = Main()
-    main.consumer_twitter()
+    #main.consumer_twitter()
     #main.list_saved_data()
+    main.analyst_tweets()
