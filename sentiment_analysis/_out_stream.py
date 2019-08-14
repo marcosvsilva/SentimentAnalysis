98fe6803651f716bae89fc6c32b77ec1e394083b
Marcos Vinicius <marcos.v.silva@live.com>
2019-08-14 16:59:04 -0300

    Consumer change to limit tweets

--- sentiment_analysis/_out_stream.py
+++ sentiment_analysis/_out_stream.py
@@ -4,18 +4,28 @@ from _persistence import Persistence
 
 
 class OutStreamListener(StreamListener):
-    def __init__(self):
+    def __init__(self, quantity_registers):
         super().__init__
         self.__persistence = Persistence()
+        self.__quantity_register = quantity_registers
+        self.__quantity = 0
 
-    def on_data(self, raw_data):
+    def __get_new_tweet(self, raw_data):
         try:
             tweet = self.format_json(raw_data)
-            print(tweet)
             self.__persistence.inset_one(tweet)
+            self.__quantity += 1
+            print(tweet)
         except BaseException as e:
-            raise Exception("Error on_data: %s" % str(e))
-            return True
+            print("Error on_data: %s" % str(e))
+
+        return True
+
+    def on_data(self, raw_data):
+        if self.__quantity > self.__quantity_register:
+            return False
+        else:
+            return self.__get_new_tweet(raw_data)
 
     def on_error(self, status_code):
         print(status_code)
